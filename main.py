05d293937e902d4e0e8c52329ce54c9743168504
Nitesh Bhandari <nitesh.bhandari@gmail.com>
2020-10-02 13:53:41 +0530

    - POST /stocks now adds stock to db

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from typing import Optional
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
 import models
 from database import SessionLocal, engine
+from models import Stock
+
+class StockRequest(BaseModel):
+    symbol: str
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -24,12 +36,17 @@ def home(request: Request):
     )
 
 @app.post("/stock")
-def create_stock():
+def create_stock(stock_request: StockRequest, db: Session = Depends(get_db)):
     """creates a stock and stores it in the database
 
     Returns:
         [type]: [description]
     """
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+    db.add(stock)
+    db.commit()
+    
     return {
         "code": "success",
         "message": "stock created",
